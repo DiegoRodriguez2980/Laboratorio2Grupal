451f309b507c1d51ea66666529b7638020e2ac5a
Piero-V <54007451+Piero-V@users.noreply.github.com>
2020-10-02 19:09:11 -0500

    finalizando

--- direct.js
+++ direct.js
@@ -1,29 +1,49 @@
-var http = require('http'),
-fs = require('fs');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = 3000;
 
-http.createServer(function (req, res) {
-    
- res.writeHead(200, {'Content-Type': 'text/html '});
+const server = http.createServer((req, res) => {
+   let filePath = path.join(
+       __dirname,
+       req.url === "/" ? "/vallume/index.html" : req.url
+   );
 
- var url = req.url;
- 
- if(url ==='/nosotros'){
-    res.write(fs.readFileSync('./nosotros.html'));
-    res.end();
- }else if(url ==='/contactos'){
-    res.write(fs.readFile('./nosotros.html')); 
-    res.end(); 
-}else if(url ==='/servicios'){
-    res.write(fs.readFile('./nosotros.html')); 
-    res.end(); 
-}else if(url ==='/catalogo'){
-    res.write(fs.readFile('./nosotros.html')); 
-    res.end(); 
- }else{
-    res.write(fs.readFileSync('./vallume/index.html')); 
-    res.end(); 
- }
+   let extName = path.extname(filePath);
+   let contentType = 'text/html';
 
-}).listen(3000, function(){
- console.log("server start at port 3000"); 
+   switch (extName) {
+       case '.css':
+           contentType = 'text/css';
+           break;
+       case '.js':
+           contentType = 'text/javascript';
+           break;
+       case '.json':
+           contentType = 'application/json';
+           break;
+       case '.png':
+           contentType = 'image/png';
+           break;
+       case '.jpg':
+           contentType = 'image/jpg';
+           break;
+   }
+
+   console.log(`File path: ${filePath}`);
+   console.log(`Content-Type: ${contentType}`)
+
+   res.writeHead(200, {'Content-Type': contentType});
+
+   const readStream = fs.createReadStream(filePath);
+   readStream.pipe(res);
+   
+});
+
+server.listen(port, (err) => {
+   if (err) {
+       console.log(`Error: ${err}`)
+   } else {
+       console.log(`Server listening at port ${port}...`);
+   }
 });
